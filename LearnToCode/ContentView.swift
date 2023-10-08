9c5193ff7536f2e65074eb589aa0f8fb18847d47
sudhir <sudhirshet2@gmail.com>
2023-10-08 11:44:34 +0530

    Initial Commit

---
+++ LearnToCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnToCode
+//
+//  Created by Sudhir Shet on 08/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnToCode/LearnToCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnToCodeApp.swift
+//  LearnToCode
+//
+//  Created by Sudhir Shet on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnToCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
